a541a2ebeccf7e66c4829e23e5a8f24b9b0580eb
Adam McFadden & james robert williams <student@epicodus.com>
2015-04-28 14:24:29 -0700

    refactor method and spec files allowing for time_passes method to control actions such as play, feed, sleep, and bath.

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -28,27 +28,41 @@ class Tamagotchi
     @stinky_level
   end
 
-  define_method(:time_passes) do
-    @food_level -= 1
-    @sleep_level -= 1
-    @activity_level -= 1
-    self.sick() if stinky_level > 3
-  end
-
-  # define_method(:time_passes) do |activity_one, activity_two|
+  # define_method(:time_passes) do
   #   @food_level -= 1
   #   @sleep_level -= 1
   #   @activity_level -= 1
   #   self.sick() if stinky_level > 3
-  #   if activity_one = play
-  #     @activity_level += 3
-  #     @sleep_level -= 1
-  #     @food_level -= 1
-  #     @stinky_level += 1
-  #     @activity_level = 10 if @activity_level > 10
-  #     "You dead" if @sleep_level < 1 || @food_level < 1
-  #   elsif activity_one = feed
+  # end
 
+  define_method(:time_passes) do |activity_one|
+    @food_level -= 1
+    @sleep_level -= 1
+    @activity_level -= 1
+    self.sick() if stinky_level > 3
+
+    if activity_one = 'play'
+      @activity_level += 3
+      @sleep_level -= 1
+      @food_level -= 1
+      @stinky_level += 1
+    elsif activity_one = 'feed'
+      @food_level += 3
+      @sleep_level -= 1
+      @activity_level -= 1
+    elsif activity_one = 'sleep'
+      @sleep_level += 3
+      @food_level -= 1
+      @activity_level -= 1
+      @stinky_level += 1
+    elsif activity_one = 'bathtime'
+      @stinky_level = 0
+    end
+
+    "You dead" if @sleep_level < 1 || @food_level < 1 || @activity_level < 1
+    @activity_level = 10 if @activity_level > 10
+    @sleep_level = 10 if @sleep_level > 10
+    @food_level = 10 if @food_level > 10
   end
 
 
@@ -72,36 +86,36 @@ class Tamagotchi
     @stinky_level = stinky_level
   end
 
-  define_method(:play) do
-    @activity_level += 3
-    @sleep_level -= 1
-    @food_level -= 1
-    @stinky_level += 1
-    @activity_level = 10 if @activity_level > 10
-    "You dead" if @sleep_level < 1 || @food_level < 1
-  end
-
-  define_method(:feed) do
-    @food_level += 3
-    @sleep_level -= 1
-    @activity_level -= 1
-  end
-
-  define_method(:sleep) do
-    @sleep_level += 3
-    @food_level -= 1
-    @activity_level -= 1
-    @stinky_level += 1
-  end
-
+  # define_method(:play) do
+  #   @activity_level += 3
+  #   @sleep_level -= 1
+  #   @food_level -= 1
+  #   @stinky_level += 1
+  #   @activity_level = 10 if @activity_level > 10
+  #   "You dead" if @sleep_level < 1 || @food_level < 1
+  # end
+  #
+  # define_method(:feed) do
+  #   @food_level += 3
+  #   @sleep_level -= 1
+  #   @activity_level -= 1
+  # end
+  #
+  # define_method(:sleep) do
+  #   @sleep_level += 3
+  #   @food_level -= 1
+  #   @activity_level -= 1
+  #   @stinky_level += 1
+  # end
+  #
   define_method(:sick) do
     @food_level -= 2
     @sleep_level -= 2
     @activity_level -= 2
   end
-
-  define_method(:bathtime) do
-    @stinky_level = 0
-  end
+  #
+  # define_method(:bathtime) do
+  #   @stinky_level = 0
+  # end
 
 end
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -12,13 +12,18 @@ describe(Tamagotchi) do
       expect(my_pet.stinky_level()).to(eq(0))
     end
   end
+
   describe("#time_passes") do
-    it("decreases the amount of food the Tamagotchi has left by 1") do
-      my_pet = Tamagotchi.new("lil dragon")
-      my_pet.time_passes()
-      expect(my_pet.food_level()).to(eq(9))
+    it('return the levels after play happens') do
+      my_pet = Tamagotchi.new('lil dragon')
+      my_pet.time_passes('play')
+      expect(my_pet.activity_level()).to(eq(10))
+      expect(my_pet.sleep_level()).to(eq(8))
+      expect(my_pet.food_level()).to(eq(8))
+      expect(my_pet.stinky_level()).to(eq(1))
     end
   end
+
   describe("#is_alive?") do
     it("return true if the food level is above zero") do
       my_pet = Tamagotchi.new("lil dragon")
@@ -32,69 +37,6 @@ describe(Tamagotchi) do
     end
   end
 
-  describe("#play") do
-    it('increases activity by 3') do
-      my_pet = Tamagotchi.new("lil dragon")
-      my_pet.set_activity_level(5)
-      my_pet.play()
-      expect(my_pet.activity_level()).to(eq(8))
-    end
-
-    it('decreases sleep by 1') do
-      my_pet = Tamagotchi.new('lil dragon')
-      my_pet.play()
-      expect(my_pet.sleep_level()).to(eq(9))
-    end
-
-    it('decreases food by 1') do
-      my_pet = Tamagotchi.new('lil dragon')
-      my_pet.play()
-      expect(my_pet.food_level()).to(eq(9))
-    end
-  end
-
-  describe("#feed") do
-    it('increases food by 3') do
-      my_pet = Tamagotchi.new("lil dragon")
-      my_pet.set_food_level(5)
-      my_pet.feed()
-      expect(my_pet.food_level()).to(eq(8))
-    end
-
-    it('decreases sleep by 1') do
-      my_pet = Tamagotchi.new('lil dragon')
-      my_pet.feed()
-      expect(my_pet.sleep_level()).to(eq(9))
-    end
-
-    it('decreases activity by 1') do
-      my_pet = Tamagotchi.new('lil dragon')
-      my_pet.feed()
-      expect(my_pet.activity_level()).to(eq(9))
-    end
-  end
-
-  describe("#sleep") do
-    it('increases sleep by 3') do
-      my_pet = Tamagotchi.new("lil dragon")
-      my_pet.set_sleep_level(5)
-      my_pet.sleep()
-      expect(my_pet.sleep_level()).to(eq(8))
-    end
-
-    it('decreases food by 1') do
-      my_pet = Tamagotchi.new('lil dragon')
-      my_pet.sleep()
-      expect(my_pet.food_level()).to(eq(9))
-    end
-
-    it('decreases activity by 1') do
-      my_pet = Tamagotchi.new('lil dragon')
-      my_pet.sleep()
-      expect(my_pet.activity_level()).to(eq(9))
-    end
-  end
-
   describe("#sick") do
     it('impacts all stats by -2') do
       my_pet = Tamagotchi.new("lil dragon")
@@ -102,13 +44,5 @@ describe(Tamagotchi) do
       expect(my_pet.activity_level()).to(eq(8))
     end
   end
-
-  describe("#bathtime") do
-    it('returns stinky_level to zero') do
-      my_pet = Tamagotchi.new('lil dragon')
-      my_pet.set_stinky_level(4)
-      my_pet.bathtime()
-      expect(my_pet.stinky_level()).to(eq(0))
-    end
-  end
+    
 end
