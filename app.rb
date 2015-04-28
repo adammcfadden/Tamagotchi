e007238ded1fac4ed42a60359e4cea49a4fbfa33
Adam McFadden & james robert williams <student@epicodus.com>
2015-04-28 16:13:29 -0700

    change app.rb file to make the program slighly more operational. Not yet fully functional.

--- app.rb
+++ app.rb
@@ -1,26 +1,28 @@
 require 'sinatra'
-# require 'sinatra/reloader'
+require 'sinatra/reloader'
 require './lib/tamagotchi'
-# also_reload './lib/**/*.rb'
+also_reload './lib/**/*.rb'
 
 get ('/') do
   erb (:index)
 end
 
-get('/tamagotchi') do
+get ('/tamagotchi') do
   @new_pet = Tamagotchi.new(params.fetch("pet_name"))
   @pet_name = @new_pet.name()
   @food_level = @new_pet.food_level()
   @sleep_level = @new_pet.sleep_level()
   @activity_level = @new_pet.activity_level()
   @stinky_level = @new_pet.stinky_level()
+  #@activity1 = params.fetch('activity_selector1')
   erb(:tamagotchi)
 end
 
-post('/activity') do
-  @activity1 = params.fetch('activity_selector')
-  @new_pet = Tamagotchi.new(@pet_name)
-  @new_pet = @new_pet.time_passes(@activity1)
+post ('/activity') do
+  @new_pet = Tamagotchi.new('tom')
+  @activity1 = params.fetch('activity_selector1')
+  @new_pet.time_passes(@activity1)
+  @pet_name = @new_pet.name()
   @food_level = @new_pet.food_level()
   @sleep_level = @new_pet.sleep_level()
   @activity_level = @new_pet.activity_level()
