89c22af5faca9455a78091d16c501fa0bd900804
Adam McFadden & james robert williams <student@epicodus.com>
2015-04-28 11:41:59 -0700

    pass first spec and update tamagotchi.rb to include pet name, food level, sleep level, and activity level.

---
+++ lib/tamagotchi.rb
@@ -0,0 +1,26 @@
+class Tamagotchi
+  @@my_pet = []
+  @@food_level = 10
+  @@sleep_level = 10
+  @@activity_level = 10
+
+  define_method(:initialize) do |pet_name|
+    @pet_name = pet_name
+  end
+
+  define_method(:name) do
+    @pet_name
+  end
+
+  define_method(:food_level) do
+    @@food_level
+  end
+
+  define_method(:sleep_level) do
+    @@sleep_level
+  end
+
+  define_method(:activity_level) do
+    @@activity_level
+  end
+end
